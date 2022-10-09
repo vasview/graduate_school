4748143bcbb26de82c93bb47847c0f19f450f4c3
vasview <vasview@gmail.com>
2022-10-09 23:05:03 +0600

    Added User profiles.

---
+++ profiles/models.py
@@ -0,0 +1,20 @@
+from io import open_code
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    middle_name = models.CharField(max_length=150, blank=True, null=True)
+    mobile = models.CharField(max_length=15, blank=True, null=True)
+    personal_number = models.SmallIntegerField(blank=True, null=True)
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
