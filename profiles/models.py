f766ad71bdda7eb932e0412d0dc3b6f7886a24eb
vasview <vasview@gmail.com>
2022-10-13 01:23:14 +0600

    Udated Profiles models.

--- profiles/models.py
+++ profiles/models.py
@@ -3,12 +3,14 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from phonenumber_field.modelfields import PhoneNumberField
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     middle_name = models.CharField(max_length=150, blank=True, null=True)
-    mobile = models.CharField(max_length=15, blank=True, null=True)
+    mobile = PhoneNumberField(blank=True, null=True)
     personal_number = models.SmallIntegerField(blank=True, null=True)
+    photo = models.ImageField(upload_to="photos/%Y/%m/%d/", blank=True, null=True)
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
