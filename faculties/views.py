3960b338932ad137bf52a6706b22d149c2d88974
vasview <vasview@gmail.com>
2022-11-04 02:31:35 +0600

    Added Supervisor main page and URLs.

--- faculties/views.py
+++ faculties/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import View, ListView, DetailView, UpdateView, CreateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
 
-# Create your views here.
+from .models import *
+from .forms import *
+
+class ListSupervisorStudents(ListView):
+    form_class = SupervisorStudentsForm
+    template_name = 'faculties/supervisor_page.html'
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST,  request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+        else:
+            return render(request, self.template_name, {'form': form})
