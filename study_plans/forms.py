b86d6b460446e1c0b3970867785141cd638d90ff
vasview <vasview@gmail.com>
2022-12-02 02:10:45 +0600

    Refactor study plan view to use CBV in them.

--- study_plans/forms.py
+++ study_plans/forms.py
@@ -1,24 +1,68 @@
 from django import forms
+from django.forms.widgets import DateInput
 
-from .models import StudyPlanType, StudyPlan, WorkType
+from .models import StudyPlanType, StudyPlan, StudyWorkScope, StudyWorkScopeDetails
 from postgraduates.models import Postgraduate
 
-class NewStudyPlan(forms.Form):
+class NewStudyPlan(forms.ModelForm):
     class Meta:
         model = StudyPlan
 
-        fields = ['postgratude', 'study_plan_type']
+        fields = ['postgraduate', 'plan_type']
 
-    postgraduate = forms.IntegerField()
-    study_plan_type = forms.ModelChoiceField(
+    plan_type = forms.ModelChoiceField(
         required=True,
         queryset=StudyPlanType.objects.all(),
     )
 
+    def __init__(self, *args, **kwargs):
+        self.postgraduate = kwargs.pop('postgraduate')
+        super(NewStudyPlan, self).__init__(*args, **kwargs)
+
+    # def save(self):
+    #     data = self.cleaned_data
+    #     # postgraduate = Postgraduate.objects.get(pk=data['postgraduate'])
+    #     study_plan = StudyPlan(postgraduate=self.postgraduate,
+    #                             plan_type=data['study_plan_type'])
+    #     study_plan.save()
+    #     return study_plan
+
+class NewStudyPlanSubject(forms.Form):
+    fields = ['deadline', 'reporting_form', 'study_work_scope']
+    pass
+
+class NewStudyPlanScopeDetails(forms.ModelForm):
+    class Meta:
+        model = StudyWorkScopeDetails
+        fields = ['study_work_scope', 'subtitle', 'summary', 'deadline', 'reporting_form']
+
+        labels = {'subtitle': 'Заголовок:', 
+                'summary': 'Краткое содержание:', 
+                'deadline': 'Срок выполнения:',
+                'reporting_form': 'Форма отчетности:'
+        }
+
+        subtitle = forms.CharField(min_length=1, max_length=250, required=False, 
+                        widget=forms.TextInput()
+        )
+        summary = forms.CharField(required=False,
+                        widget=forms.Textarea(attrs={ 'class': 'form-control', 'rows':'3', })
+        )
+        deadline = forms.DateField(required=True,
+                        widget=DateInput(attrs={ 'type': 'date', 'class': 'form-control' })
+        )
+        reporting_form = forms.CharField(max_length=250, required=False,
+                        widget=forms.TextInput(attrs={'class': 'form-control'})
+        )
+
     def save(self):
         data = self.cleaned_data
-        postgraduate = Postgraduate.objects.get(pk=data['postgraduate'])
-        study_plan = StudyPlan(postgraduate=postgraduate,
-                                plan_type=data['study_plan_type'])
-        study_plan.save()
-        return study_plan
+        scope_details = StudyWorkScopeDetails(study_work_scope=data['study_work_scope'],
+                                subtitle=data['subtitle'],
+                                summary=data['summary'],
+                                deadline=data['deadline'],
+                                reporting_form=data['reporting_form'])
+        scope_details.save()
+        return scope_details
+
+
--- study_plans/views.py
+++ study_plans/views.py
@@ -4,66 +4,70 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy
 from django.shortcuts import get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
 
 from main.views import StudentMenuView
 from .forms import *
 from .models import *
 
 
-class ListStudyPlans(LoginRequiredMixin, StudentMenuView, View):
+class ListStudyPlans(LoginRequiredMixin, StudentMenuView, ListView):
+    model = StudyPlan
     template_name = 'study_plans/student_study_plans.html'
+    context_object_name = 'study_plans'
+    login_url = '/login/'
 
-    def get_context_data(self, request, *args,**kwargs):
+    def get_context_data(self, *args,**kwargs):
         context =  super().get_context_data(*args, **kwargs)
-        user = request.user
-        student = user.students.last()
-        study_plans = student.study_plans.all()
-        context['user'] = user
-        context['student'] = student
-        context['study_plans'] = study_plans
         return context
 
-    def get(self,request,*args, **kwargs):
-        context = self.get_context_data(request)
-        return render(request, self.template_name, context)
+    def get_queryset(self):
+        user = self.request.user
+        student = user.students.last()
+        return StudyPlan.objects.filter(postgraduate=student)
 
-class ShowStudyPlan(StudentMenuView, View):
+
+class ShowStudyPlan(LoginRequiredMixin, StudentMenuView, DetailView):
+    model = StudyPlan
     template_name = 'study_plans/show_study_plan.html'
+    pk_url_kwarg = 'id'
+    context_object_name = 'study_plan'
+    login_url = '/login/'
 
-    def get_context_data(self, request, *args,**kwargs):
+    def get_context_data(self, *args,**kwargs):
         context =  super().get_context_data(*args, **kwargs)
-        user = request.user
-        student = user.students.last()
-        context['user'] = user
-        context['student'] = student
         return context
 
-    def get_study_plan(self):
-        return get_object_or_404(StudyPlan, pk=self.kwargs['id'])
 
-    def get(self, request, *args, **kwargs):
-        context = self.get_context_data(request)
-        context["study_plan"] = self.get_study_plan()
-        return render(request, self.template_name, context)
-
-class CreateStudyPlan(StudentMenuView, View):
+class CreateStudyPlan(LoginRequiredMixin, StudentMenuView, CreateView):
     form_class = NewStudyPlan
     template_name = 'study_plans/create_study_plan.html'
+    login_url = '/login/'
+    success_url = reverse_lazy("student_study_plans:index")
 
-    def get_context_data(self, request, *args,**kwargs):
-        context =  super().get_context_data(*args, **kwargs)
-        user = request.user
-        student = user.students.last()
-        context['user'] = user
-        context['student_id'] = student.id
+    # def get_success_url(self):
+    #     return redirect("student_study_plans:index")
+
+    def get_context_data(self, *args,**kwargs):
+        context = super(CreateStudyPlan, self).get_context_data(*args, **kwargs)
         return context
+
+    def get_initial(self, *args, **kwargs):
+        initial = super().get_initial(**kwargs)
+        user = self.request.user
+        initial['postgraduate'] = user.students.last()
+        return initial
+    
+    def get_form_kwargs(self, *args, **kwargs):
+        kwargs = super(CreateStudyPlan, self).get_form_kwargs(*args, **kwargs)
+        user = self.request.user
+        kwargs['postgraduate'] = user.students.last()
+        return kwargs
 
     def create_study_plan_work_scopes(self, study_plan_work):
         work_scopes = WorkScopeType.objects.filter(work_type=study_plan_work.work_type).order_by('sort')
         for scope in work_scopes:
-            study_plan_work.study_work_scopes.create(work_scope=scope, 
-                                                    subtitle=scope.title, 
-                                                    is_section_title=True)
+            study_plan_work.study_work_scopes.create(work_scope=scope)
 
     def create_study_plan_works(self, study_plan):
         if study_plan.plan_type.year_of_study == 0:
@@ -75,9 +79,65 @@ class CreateStudyPlan(StudentMenuView, View):
             study_plan_work = study_plan.study_plan_works.create(work_type=work)
             self.create_study_plan_work_scopes(study_plan_work)
 
+    def form_valid(self, form):
+        study_plan = form.save()
+        self.create_study_plan_works(study_plan)
+        return HttpResponseRedirect(self.success_url)
+
+
+class AddStudyWorkScopeSubject(LoginRequiredMixin, StudentMenuView, View):
+    template_name = 'study_plans/add_work_scope_subject.html'
+    login_url = '/login/'
+
+    def get_context_data(self, request, *args,**kwargs):
+        context =  super().get_context_data(*args, **kwargs)
+        # user = request.user
+        # student = user.students.last()
+        # context['user'] = user
+        # context['student_id'] = student.id
+        return context
+
     def get(self, request, *args, **kwargs):
+        # context = self.get_context_data(request)
+        # form = self.form_class(initial={'postgraduate': context['student_id']})
+        form = NewStudyPlanScopeDetails()
+        # context['form'] = form
+        context = {'form': form}
+        return render(request, self.template_name, context)
+
+    # def post(self, request, *args, **kwargs):
+    #     context = self.get_context_data(request)
+    #     form = self.form_class(request.POST)
+    #     if form.is_valid():
+    #         study_plan = form.save()
+    #         self.create_study_plan_works(study_plan)
+    #         return redirect("student_study_plans:index")
+    #     else:
+    #         context['form'] = form
+    #         return render(request, self.template_name, context)
+
+
+class AddStudyWorkScopeDetails(LoginRequiredMixin, StudentMenuView, View):
+    form_class = NewStudyPlanScopeDetails
+    template_name = 'study_plans/add_work_scope_details.html'
+    login_url = '/login/'
+
+    def get_study_work_scope(self):
+        return get_object_or_404(StudyWorkScope, pk=self.kwargs['id'])
+
+    def get_context_data(self, request, *args,**kwargs):
+        context =  super().get_context_data(*args, **kwargs)
+        # user = request.user
+        # student = user.students.last()
+        # context['user'] = user
+        # context['student_id'] = student.id
+        return context
+
+    def get(self, request, *args, **kwargs):
+        work_scope = self.get_study_work_scope()
         context = self.get_context_data(request)
-        form = self.form_class(initial={'postgraduate': context['student_id']})
+
+        form = self.form_class(initial={'study_work_scope': work_scope.id })
         context['form'] = form
         return render(request, self.template_name, context)
 
@@ -85,9 +145,29 @@ class CreateStudyPlan(StudentMenuView, View):
         context = self.get_context_data(request)
         form = self.form_class(request.POST)
         if form.is_valid():
-            study_plan = form.save()
-            self.create_study_plan_works(study_plan)
-            return redirect("student_study_plans:index")
+            scope_details = form.save()
+            plan_id = scope_details.study_work_scope.study_plan_work.study_plan_id
+            # return reverse_lazy("student_study_plans:show_study_plan", kwargs={'id': plan_id})
+            return redirect("student_study_plans:show_study_plan", id=plan_id)
         else:
             context['form'] = form
             return render(request, self.template_name, context)
+
+class DeleteStudyWorkScopeDetails(LoginRequiredMixin, StudentMenuView, DeleteView):
+    model = StudyWorkScopeDetails
+    pk_url_kwarg = 'id'
+    template_name = 'confirm_delete.html'
+    login_url = '/login/'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Вы точно хотите удалить краткое содержание и объем работы?'
+        return context
+
+    def get_object(self):
+        return get_object_or_404(StudyWorkScopeDetails, pk=self.kwargs['id'])
+
+    def get_success_url(self):
+        scope_details = self.get_object()
+        plan_id = scope_details.study_work_scope.study_plan_work.study_plan_id
+        return reverse_lazy('student_study_plans:show_study_plan', kwargs={'id': plan_id})
