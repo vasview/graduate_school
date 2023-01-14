b5d456d59fb22f58a43b90ce91328ff849520833
Vasily Toropchin <vasview@gmail.com>
2023-01-15 02:53:10 +0600

    Move backend applciation to src folder. Updated relevant docker and docker-compose files.

--- docker/backend/entrypoint.sh
+++ docker/backend/entrypoint.sh
@@ -2,8 +2,13 @@
 
 set -e
 
-python manage.py migrate
+until cd ../../app
+do
+    echo "Waiting for server volume..."
+done
 
-python manage.py collectstatic --noinput
+python ./manage.py migrate
+
+python ./manage.py collectstatic --noinput
 
 gunicorn config.wsgi --bind 0.0.0.0:8000 --workers 2 --threads 2
--- src/config/settings.py
+++ src/config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-sp4*3d0lln(j7y&8leay=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = str(os.environ.get('DEBUG')) == '1'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 if not DEBUG:
     ALLOWED_HOSTS += os.environ.get('ALLOWED_HOSTS').split(',')
 
